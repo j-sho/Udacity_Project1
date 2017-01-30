c68397f172693d486556836d21a46091f773410f
Julia <shor.jul@gmail.com>
2017-01-30 15:41:48 +0300

    Update media.py
    I provided information to the user about the class I am implementing

--- media.py
+++ media.py
@@ -2,13 +2,31 @@ import webbrowser
 
 
 class Video():
-	# def __init__(self, movie_title, movie_duration):
-	# 	self.title = movie_title
-	# 	self.duration = movie_duration
+
+	"""
+	Video class object that provides a way to store movie related information
+	and will be used in the entertainment_center.py file
+
+	Attributes:
+
+    title:
+        It is the movie (or Tv series) title from IMDB  (http://www.imdb.com/)
+    storyline:
+        A short discription of the movie (or Tv series) from IMDB
+        (http://www.imdb.com/)
+    poster_image_url:
+        The URL address of a poster image of the movie (or TV series) from IMDB
+        (http://www.imdb.com/) or other recursos
+    trailer_youtube_url:
+        A watch link on official trailer from youtube (https://www.youtube.com)
+    """
+
 	def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+		"""Initialises Video class and provides a way to store related information"""
 		self.title = movie_title
 		self.storyline = movie_storyline
 		self.poster_image_url = poster_image
 		self.trailer_youtube_url = trailer_youtube
 	def show_trailer(self):
+		"""Plays the movie trailer in the web browser"""
 		webbrowser.open(self.trailer_youtube_url, new=2, autoraise=True)
